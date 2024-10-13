31700235ba96a7c7148923bdd64c714cc8187cb3
govind01a <govind01pna@gmail.com>
2024-10-13 12:58:34 +0530

    Added Theme Provider

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,21 @@ import './App.css';
 import AppRoutes from './Routes/AppRoutes';
 import Header from './Components/header/Header';
 import Footer from './Components/footer/Footer';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme();
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <AppRoutes />
-      <Footer />
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <div className="App">
+        <Header />
+        <AppRoutes />
+        <Footer />
+      </div>
+    </ThemeProvider>
   );
 }
 
